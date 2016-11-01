c89edbd9194dbebb51bfc17f477b24bdf355a5dc
LordTandy <mat.tandy@hotmail.co.uk>
2016-11-01 19:07:25 +0000

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var stage = document.getElementById('st').innerHTML
+ if (stage == "Day") {
+   document.getElementById('st').innerHTML = "Night";
+ } else {
+   document.getElementById('st').innerHTML = "Day";
+ }
+}
